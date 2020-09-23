d99ce020f2224c03e22169f2e71f6a39e4d38341
JakubWronka <jakubwronka7@gmail.com>
2020-09-23 19:54:28 +0200

    trip model start

---
+++ app/src/main/java/com/example/gooddriving/TripModel.kt
@@ -0,0 +1,10 @@
+package com.example.gooddriving
+
+class TripModel {
+    private var distance: Float = 0.0F
+    private var date: String = ""
+    private var avgSpeed: Float = 0.0F
+    private var maxSpeed: Float = 0.0F
+    private var locations: MutableList<Float> = ArrayList()
+
+}
