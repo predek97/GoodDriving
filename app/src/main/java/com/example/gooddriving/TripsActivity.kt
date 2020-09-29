7959b08b9e35a46171e1713c5d4f45b5394918c4
JakubWronka <jakubwronka7@gmail.com>
2020-09-29 21:44:26 +0200

    small update - toast

--- app/src/main/java/com/example/gooddriving/TripsActivity.kt
+++ app/src/main/java/com/example/gooddriving/TripsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gooddriving
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TripsActivity : BasicLayoutActivity() {
@@ -13,5 +15,10 @@ class TripsActivity : BasicLayoutActivity() {
         textMessage = findViewById(R.id.message)
         navView.selectedItemId = R.id.navigation_trips
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        val tripOneButton: Button = findViewById(R.id.trip1_button)
+        tripOneButton.setOnClickListener {
+            Toast.makeText(this, "Trip one info expanded", Toast.LENGTH_SHORT).show()
+        }
     }
 }
