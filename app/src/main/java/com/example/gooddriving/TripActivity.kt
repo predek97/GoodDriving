2355b7b62714a8e091a758ddc61746ad7af7a6b2
JakubWronka <jakubwronka7@gmail.com>
2020-10-25 19:00:06 +0100

    added map fragment to TripActivity

--- app/src/main/java/com/example/gooddriving/TripActivity.kt
+++ app/src/main/java/com/example/gooddriving/TripActivity.kt
@@ -3,8 +3,14 @@ package com.example.gooddriving
 import android.app.ActionBar
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class TripActivity : BasicLayoutActivity() {
+class TripActivity : BasicLayoutActivity(), OnMapReadyCallback {
 
     companion object {
         const val tripName = "default trip name"
@@ -20,8 +26,28 @@ class TripActivity : BasicLayoutActivity() {
 
         var textView: TextView = findViewById(R.id.message1)
         textView.setText(tripNameToDisplay)
+
+        if (getString(R.string.google_maps_key).isEmpty()) {
+            Toast.makeText(this, "Add your own API key in MapWithMarker/app/secure.properties as MAPS_API_KEY=YOUR_API_KEY", Toast.LENGTH_LONG).show()
+        }
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.tripMap) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
     }
 
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap?.apply {
+            val sydney = LatLng(-33.852, 151.211)
+            addMarker(
+                MarkerOptions()
+                    .position(sydney)
+                    .title("Marker in Sydney")
+            )
+        }
+    }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
