ca0b1f0a4e019f77615a638d5931d87be09ddacd
predek <predek00@gmail.com>
2020-12-05 16:51:05 +0100

    add accelaration tracking and violation creating

--- app/src/main/java/com/example/gooddriving/DashboardActivity.kt
+++ app/src/main/java/com/example/gooddriving/DashboardActivity.kt
@@ -1,19 +1,25 @@
 package com.example.gooddriving
 
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
-import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import android.view.View
-import android.widget.Button
+import android.widget.TextView
 import android.widget.ToggleButton
+import com.example.gooddriving.db.Violation
 import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.reflect.KFunction1
 
 
 class DashboardActivity : BasicLayoutActivity() {
@@ -21,10 +27,16 @@ class DashboardActivity : BasicLayoutActivity() {
     private var tripStarted: Boolean = false
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var tripService: TripService
-    protected lateinit var locations : ArrayList<Location>;
+    protected lateinit var locations : ArrayList<Location>
+    protected lateinit var violations : ArrayList<Violation>
     protected var distanceCovered : Array<Double> = arrayOf(0.0)
+    protected var violationContinous = false
 
-    private var locationManager: LocationManager? = null
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensor: Sensor
+    private lateinit var textView : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +48,12 @@ class DashboardActivity : BasicLayoutActivity() {
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
         val toggleButton: ToggleButton = findViewById(R.id.toggleButton)
         toggleButton.setOnClickListener(onStartButtonSelectedListener)
+        textView = findViewById(R.id.textView)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         tripService = TripService(this)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
     }
 
     protected val onStartButtonSelectedListener = View.OnClickListener {
@@ -55,8 +70,10 @@ class DashboardActivity : BasicLayoutActivity() {
     }
 
     protected fun startTrip() {
-        locations = ArrayList();
+        locations = ArrayList()
+        violations = ArrayList()
         locationCallback = MyLocationCallback(this.locations, this.distanceCovered)
+        accelerometerListener = MyAccelerometerListener(::accelerometerCallback)
         try {
             val locationRequest = LocationRequest.create()?.apply {
                 interval = 10000
@@ -66,6 +83,7 @@ class DashboardActivity : BasicLayoutActivity() {
             fusedLocationClient.requestLocationUpdates(locationRequest,
                 locationCallback,
                 Looper.getMainLooper())
+            sensorManager.registerListener(accelerometerListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
         }
         catch(e : SecurityException ) {
             //TO-DO handle lack of permissions
@@ -73,10 +91,48 @@ class DashboardActivity : BasicLayoutActivity() {
     }
 
     protected fun stopTrip() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        sensorManager.unregisterListener(accelerometerListener)
         tripService.save(generateTrip())
     }
 
+    protected fun accelerometerCallback(accelerationAxes : Triple<Float, Float, Float>) {
+        val (x, y, z) = accelerationAxes
+        if(!violationContinous && checkViolationConditions(accelerationAxes)) {
+            createViolation(z, x)
+        }
+        updateViolationContinous(accelerationAxes)
 
+    }
+
+    protected fun createViolation(linearGForce : Float, lateralGForce : Float) {
+        val lastLocation = locations.last()
+        var violation = Violation(
+            speed = lastLocation.speed.toDouble(),
+            lateralGForce = linearGForce.toDouble(),
+            linearGForce = lateralGForce.toDouble(),
+            latitude = lastLocation.latitude,
+            longitude = lastLocation.longitude,
+            timestamp = lastLocation.time,
+            correspondingVTripId = 0
+        )
+        violations.add(violation)
+    }
+
+    protected fun checkViolationConditions(accelerationAxes: Triple<Float, Float, Float>) : Boolean {
+        val (x, y, z) = accelerationAxes
+        return (z > 5 || z < -5)
+    }
+
+    protected fun updateViolationContinous(accelerationAxes: Triple<Float, Float, Float>) {
+        val (x, y, z) = accelerationAxes
+        if(checkViolationConditions(accelerationAxes) && !violationContinous) {
+            violationContinous = true
+        }
+        else if(!checkViolationConditions(accelerationAxes) && violationContinous) {
+            violationContinous = false
+        }
+    }
 
     protected fun generateTrip(): TripModel {
         val dateOfTrip = Date(locations.first().time)
@@ -91,7 +147,7 @@ class DashboardActivity : BasicLayoutActivity() {
         avgSpeed /= locations.size
         return TripModel(
             locationList = locations,
-            violationList = ArrayList(),
+            violationList = violations,
             grade = 0.0,
             dateOfTrip = dateFormat.format(dateOfTrip),
             distanceCovered = this.distanceCovered[0],
@@ -102,8 +158,17 @@ class DashboardActivity : BasicLayoutActivity() {
         )
     }
 
+
     protected lateinit var locationCallback: LocationCallback
 
+    private lateinit var  accelerometerListener: SensorEventListener
+
+    class MyAccelerometerListener(val callback: KFunction1<@ParameterName(name = "coordinates") Triple<Float, Float, Float>, Unit>) : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor, acc: Int) {}
+        override fun onSensorChanged(event: SensorEvent) {
+            callback(Triple(event.values[0], event.values[1], event.values[2]))
+    }
+    }
     class MyLocationCallback constructor(var locations : ArrayList<Location>, var distanceCovered : Array<Double>) : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult?) {
             super.onLocationResult(locationResult)
@@ -125,6 +190,7 @@ class DashboardActivity : BasicLayoutActivity() {
             }
         }
     }
+
 
 
 }
